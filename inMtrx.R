856dfbc653c09658076948fa5804000873a45f08
Jose Sousa <jm.alves.sousa@gmail.com>
2018-01-09 15:29:58 +0000

    Initial Commit R Study Asus

---
+++ inMtrx.R
@@ -0,0 +1,50 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  matrInv <- NULL
+  
+  ## This function sets the value of x, that is in the parent environment with y
+  ## using the deep assignment operator and makes matr = Null, in practice resets 
+  ## the atribute of the object.
+  getx <- function() x
+  set <- function(y){
+    x <<- y
+    matrInv <<- NULL
+  }
+  
+  get <- function(){
+    matrInv
+  }
+  
+  setInv <- function(inverse){
+    matrInv <<- inverse
+  }
+  
+  getInV <- function(){
+    matrInv
+  }
+  
+  list(set = set, get = get, setInv = setInv, getInV = getInV, getx = getx)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  
+  if(!is.null(inv)){
+    print("Getting data ...")
+    return(inv)
+  }
+  
+  m <- x$get()
+  inv <- solve(m)
+  x$setInv(inv)
+  inv
+  
+}
